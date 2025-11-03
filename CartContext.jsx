7416651ddb3ba6ad56277fc676a8b11f45e2acd7
= <=>
2025-11-03 23:39:33 +0500

    qty add

--- CartContext.jsx
+++ CartContext.jsx
@@ -3,15 +3,29 @@ import { createContext, useState } from "react";
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-    const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState([]);
 
-    function addToCart(product) {
-        setCart([...cart, product]);
-    }
+  function addToCart(product) {
+    setCart((prevCart) => {
+      const existingItem = prevCart.find((item) => item.id === product.id);
 
-    return (
-        <CartContext.Provider value={{ cart, addToCart }}>
-            {children}
-        </CartContext.Provider>
-    );
+      if (existingItem) {
+        // agar product already cart me hai to quantity badhao
+        return prevCart.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      } else {
+        // naya product add karo with quantity 1
+        return [...prevCart, { ...product, quantity: 1 }];
+      }
+    });
+  }
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
 };
